615d43d26f806b5499e94afad9048a2668bd5d58
Your Name <you@example.com>
2023-09-06 11:42:54 +0100

    health check added to route handler

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,21 +1,11 @@
 import express, { Router } from "express";
-import { Client } from "pg";
-import { useErrorHandler } from "../utils/errorHandler";
 
-export default function createRootRouter(client: Client): Router {
+export default function createRootRouter(): Router {
     const router = express.Router();
 
     router.get("/", async (_req, res) => {
         res.json({ msg: "Hello! There's nothing interesting for GET /" });
     });
-
-    router.get(
-        "/health-check",
-        useErrorHandler(async (_req, res) => {
-            await client.query("select now()");
-            res.status(200).send("system ok");
-        })
-    );
 
     return router;
 }
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import { Client } from "pg";
 import createLeaderboardRouter from "./routes/leaderboard";
 import createRootRouter from "./routes/root";
+import createHealthCheckRouter from "./routes/healthCheck";
 import { getEnvVarOrFail } from "./support/envVarUtils";
 import { setupDBClientConfig } from "./support/setupDBClientConfig";
 import { errorHandler } from "./utils/errorHandler";
@@ -20,8 +21,9 @@ app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cors());
 
-app.use("/", createRootRouter(client));
+app.use("/", createRootRouter());
 app.use("/leaderboard", createLeaderboardRouter(client));
+app.use("/health-check", createHealthCheckRouter(client));
 
 app.use(errorHandler);
 
